9412baa386028fb8b0b55db1f63cb844f0297818
taemath <hirotaka081630@gmail.com>
2024-07-09 05:04:16 +0900

    feat(lib/interface) add TODO

--- chapter01/src/interface.rs
+++ chapter01/src/interface.rs
@@ -12,6 +12,7 @@ pub trait List<T> {
     fn size(&self) -> usize;
     fn get(&self, i: usize) -> Option<T>;
     fn set(&mut self, i: usize, x: T) -> Option<T>;
+    // TODO: Result<T, E>
     fn add(&mut self, i: usize, x: T);
     fn remove(&mut self, i: usize) -> Option<T>;
 }
